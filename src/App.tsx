d350a5977ca27863e7d81f6b4f725e9534db556e
Sergio <Sergio.ltr96@gmail.com>
2022-11-02 15:43:30 +0100

    Sample class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Layout></Layout>
       </header>
     </div>
   );
