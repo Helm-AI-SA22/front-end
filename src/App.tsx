56bdaaf8b24a5ab42f55a00d47afdfda1b9955dc
Sergio <Sergio.ltr96@gmail.com>
2022-11-02 17:27:15 +0100

    Mui responsive AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Layout  from './components/Layout/Layout';
+import AppBar from './components/AppBar/AppBar';
+
 
 function App() {
   return (
     <div className="App">
+        <AppBar></AppBar>
         <Layout></Layout>
     </div>
   );
