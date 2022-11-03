a604ec774b836116929e68165224c1f6d7c14e61
Sergio <Sergio.ltr96@gmail.com>
2022-11-03 18:16:52 +0100

    Home Page sketch

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,28 +1,36 @@
 import React, {Component } from 'react';
 import './SearchBar.css';
 
-import {Autocomplete, TextField} from '@mui/material';
+import {Autocomplete, TextField, Button} from '@mui/material';
+import { Search } from '@mui/icons-material' 
 
 interface AutocompleteOption {
     label: string;
   }
 
 const options = [
-    { label: 'The Godfather', id: 1 },
-    { label: 'Pulp Fiction', id: 2 },
+    { label: 'Siamese Network', id: 1 },
+    { label: 'Social Robotics', id: 2 },
   ];
 
 export default class SearcBar extends Component {
     render(){ 
         return (
-            <div className="SearcBar">
+            <div className="search-bar">
                 <Autocomplete
+                    freeSolo={true}
                     disablePortal
-                    id="combo-box-demo"
+                    className='search-autocomplete'
                     options={options}
                     sx={{ width: 300 }}
-                    renderInput={(params) => <TextField {...params} label="Movie" />}
+                    renderInput={(params) => <TextField {...params} label="Research Topic" />}
                 />
+                <Button variant="contained" endIcon={<Search />} className="search-button" onClick={() => {
+                  // @TODO call query api and change route
+                  alert('searched');
+                }}>
+                    Search
+                </Button>
             </div>
           );
     }
--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -2,12 +2,24 @@ import React, {Component } from 'react';
 
 import './Home.css';
 import SearchBar from '../../components/SearchBar/SearchBar';
+import bluLogo from '../../assets/logo/black.png';
 
+import { Container } from '@mui/material';
 
 export default class HomePage extends Component {
     render(){
         return (
-            <SearchBar/>
+            <Container maxWidth="xl" className="page">
+                <div className="logo-container">
+                    <img src={bluLogo} alt="logo" />
+                </div>
+                <div className='title'>
+                    Helm
+                </div>
+                <div className="searchbarbar-container">
+                    <SearchBar/>
+                </div>
+            </Container>
           );
     }
 }
