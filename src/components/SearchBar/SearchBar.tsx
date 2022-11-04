c2406e874a57bfaafc580b79e1b78465589672a1
Sergio <Sergio.ltr96@gmail.com>
2022-11-04 17:44:08 +0100

    Chip Searchbar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './SearchBar.css';
 
-import {Autocomplete, TextField, Button} from '@mui/material';
+import {Autocomplete, TextField, Button, Chip} from '@mui/material';
 import { Search } from '@mui/icons-material' 
 import Switch from '@mui/material/Switch';
 
@@ -11,7 +11,7 @@ import { TopicModelingAlgorithm } from '../../api/interfaces';
 const options = ['Siamese Network',  'Social Robotics'];
 
 const SearcBar = () => {
-    const [value, setValue] = React.useState<string | null>(options[0]);
+    const [value, setValue] = React.useState<string[]>(options);
     const [inputValue, setInputValue] = React.useState('');
     const [checked, setChecked] = React.useState(false);
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,27 +22,35 @@ const SearcBar = () => {
         <div >
           <div className="search-bar">
               <Autocomplete
+                  multiple
                   value={value}
-                  onChange={ (event: any, newValue: string | null ) => {
-                    setValue(newValue);
+                  onChange={(_, newValue) => {
+                    setValue(newValue)
                   }}
+                  defaultValue={[]} 
                   inputValue={inputValue}
-                  onInputChange={(event, newInputValue) => {
+                  onInputChange={(_, newInputValue) => {
                     setInputValue(newInputValue);
                   }}
+
                   freeSolo={true}
                   disablePortal
                   className='search-autocomplete'
                   options={options}
                   sx={{ width: 500 }}
-                  renderInput={(params) => <TextField {...params} label="Research Topic" />}
+                  renderInput={(params) => <TextField {...params} variant="filled" label="Research Topic" placeholder="Bibliometry"/>}
+                  renderTags={(value: readonly string[], getTagProps) =>
+                    value.map((option: string, index: number) => (
+                      <Chip variant="outlined" label={option} {...getTagProps({ index })} />
+                    ))
+                  }
               />
-
               <Button variant="contained" endIcon={<Search />} className="search-button" onClick={async () => {
-                if(value){
-                  alert(`You are searching: ${value}.  Research type: ${checked ? 'fast': 'slow'}.`);
+                console.log(value)
+                if(value.length){
+                  alert(`You are searching: ${value.map((kw => kw + ' '))}.  Research type: ${checked ? 'fast': 'slow'}.`);
                   await searchAPI({
-                    keywords: [value], 
+                    keywords: value, 
                     type: (checked ? 'LDA' : 'BERT') as TopicModelingAlgorithm});
                   //@redirect
                 }
