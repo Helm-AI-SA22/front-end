808c3d50e7ff8e655b24f820e18f5464f518f86b
VeronicaPistolesi <v.pistolesi6@studenti.unipi.it>
2022-11-23 23:26:41 +0100

    home and interfaces little changes

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -7,6 +7,7 @@ import {Container, Box, Toolbar} from '@mui/material';
 import {Typography, Divider} from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Logo from '../../assets/logo/primary2.png';
+import Helm from '../../assets/logo/helm.png';
 
 import SearchBar from '../../components/SearchBar/SearchBar';
 // import PageFooter from '../../components/Footer/Footer';
@@ -44,30 +45,38 @@ export default class HomePage extends Component {
                 {/*<Container maxWidth="md">
                     
                 </Container>*/}
-                <Container maxWidth="sm" sx={{display: 'flex', FlexDirection: 'row', alignItems: 'center'}}>
+                <Container maxWidth="sm" sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
                     <Box
                         component="img"
                         sx={{
-                        height: 200,
-                        float: 'right'
+                        height: 150,
+                       
                         }}
                         alt="Your logo."
                         src={Logo}
                     />
+                    <Box
+                        component="img"
+                        sx={{
+                        height: 50,
+                        
+                        }}
+                        alt="Helm.ai"
+                        src={Helm}
+                    />
                     <Typography  variant="h5" align="center"  color="text.secondary" sx={{fontWeight: 'bold', minWidth: '40%', marginTop: 5}} paragraph>
-                    {'In the sea of the academic knowledge,'} <br/> 
-                    {'let HELM guide you to destination ports.'}
-                    <Button sx={{pt:2}} variant="text" >Read more about us</Button>
-                    </Typography>
-                    
+                    {'Navigate through research oceans'} <br />
+                        <Button sx={{pt:2}} variant="text" >Read more about us</Button>
+                    </Typography>   
                 </Container>
                 </Box>
+
                 <Box sx={{bgcolor: 'background.paper', pt: 2, pb: 3}}>
                 <Container sx={{py: 0}} maxWidth="md">
-                    <Divider />
+                    {/*<Divider />
                     <Typography variant="subtitle1" align="left" color="text.secondary" sx={{ ml:15, fontWeight: 'bold', pt:5, pb:1}} paragraph>
                     Choose your interest and start exploring
-                    </Typography>
+                    </Typography>*/}
                     <SearchBar />
                 </Container>
                 </Box>
